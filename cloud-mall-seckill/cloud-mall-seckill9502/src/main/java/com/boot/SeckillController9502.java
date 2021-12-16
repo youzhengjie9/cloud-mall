c720cdadab12be144542c35cdaed149d3fb7995b
ms666 <1550324080@qq.com>
2021-12-16 18:00:33 +0800

    cloud-mall 创建秒杀模块

---
+++ cloud-mall-seckill/cloud-mall-seckill9502/src/main/java/com/boot/SeckillController9502.java
@@ -0,0 +1,28 @@
+package com.boot;
+import com.boot.config.ScanClassProperties;
+import com.boot.config.SwaggerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 秒杀模块
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Import({SwaggerConfig.class}) //导入swaggerConfig的配置类
+@EnableSwagger2  //开启Swagger2
+@EnableConfigurationProperties(ScanClassProperties.class)
+@EnableFeignClients
+@EnableDiscoveryClient
+public class SeckillController9502 {
+
+  public static void main(String[] args) {
+      SpringApplication.run(SeckillController9502.class,args);
+  }
+
+}
